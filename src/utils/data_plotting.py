106b4d6b1c817034651e0953486e85d0a0a787f5
bakanaouji <abeken1224@yahoo.co.jp>
2018-07-08 20:24:39 +0900

    add legend to plot

--- src/utils/data_plotting.py
+++ src/utils/data_plotting.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-def plot_each_data(folder_name, file_name, y_min, y_max, data=None):
+def plot_each_data(folder_name, file_name, y_min, y_max, data=None, legend=None):
     if data is None:
         data = pd.DataFrame.from_csv(folder_name + '/' + file_name + '.csv')
     plt.plot(data)
@@ -11,13 +11,15 @@ def plot_each_data(folder_name, file_name, y_min, y_max, data=None):
     plt.xscale('log')
     plt.xlim(0, len(data.index))
     plt.ylim(y_min, y_max)
+    if legend is not None:
+        plt.legend(legend, loc='upper left')
     plt.savefig(folder_name + '/' + file_name + '.pdf')
     plt.close()
 
 
 def plot_summarized_data(folder_name, file_name, y_min, y_max):
-    files = os.listdir(folder_name)
-    files = [file for file in files if file[-4:] != '.pdf' and file[:1] != '.']
+    file_names = os.listdir(folder_name)
+    files = [file for file in file_names if file[-4:] != '.pdf' and file[:1] != '.']
 
     # summarize data
     df = [pd.DataFrame.from_csv(folder_name + '/' + file
@@ -29,4 +31,4 @@ def plot_summarized_data(folder_name, file_name, y_min, y_max):
 
     # plot data
     plot_each_data(folder_name, 'summarized_maen_' + file_name, y_min, y_max,
-                   df_concat)
+                   df_concat, file_names)
